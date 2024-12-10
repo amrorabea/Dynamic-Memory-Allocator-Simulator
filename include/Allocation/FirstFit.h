6696c0679662d9b456de1aadcbd1a9610954dfa5
Ahmed Abbas <ah.abbas333@gmail.com>
2024-12-10 03:24:51 +0200

    FIX: Techniques (deallocate), change processes storage type from vector to map.

--- include/Allocation/FirstFit.h
+++ include/Allocation/FirstFit.h
@@ -1,13 +1,14 @@
 #ifndef FIRSTFIT_H
 #define FIRSTFIT_H
 
+
 #include "AllocationTechnique.h"
 
 class FirstFit : public AllocationTechnique {
 public:
     static bool allocate(Process &process, std::vector<Partition> &partitions);
 
-    static bool deallocate(const int &process_id, std::vector<Process> &processes, std::vector<Partition> &partitions);
+    static bool deallocate(const int &process_id, std::map<int, Process> &processes, std::vector<Partition> &partitions);
 
     void format(std::vector<Process> &processes, std::vector<Partition> &partitions);
 };
